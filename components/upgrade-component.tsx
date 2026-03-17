fa5df9084b6d39987aea9d9ba613d1b433cef714
Rakesh K <codersgyan@gmail.com>
2026-03-17 12:00:56 +0100

    integrate the polar

--- components/upgrade-component.tsx
+++ components/upgrade-component.tsx
@@ -1,14 +1,23 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 export function UpgradeComponent() {
   return (
     <div className="absolute left-1/2 -translate-x-1/2">
       <Button
-        onClick={async () => {}}
+        onClick={async () => {
+          await authClient.checkout({
+            products: [
+              "d4692ac4-fd8f-4ab7-a73a-66a1403f9627",
+            ],
+            // OR
+            // if "products" in passed in the checkout plugin's config, you may pass the slug
+            slug: "Pro",
+          });
+        }}
         variant="default"
-        className="rounded-full bg-[#373669] border-[#3e3e4a] text-white hover:bg-[#373669]/60 text-[12px] h-8 px-4 font-medium"
-      >
+        className="rounded-full bg-[#373669] border-[#3e3e4a] text-white hover:bg-[#373669]/60 text-[12px] h-8 px-4 font-medium">
         ✦ Get Plus
       </Button>
     </div>
--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,23 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import { schema } from "@/db/schema/auth-schema";
 
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+} from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
+
+const polarClient = new Polar({
+  accessToken: process.env.POLAR_ACCESS_TOKEN,
+
+  // Use 'sandbox' if you're using the Polar Sandbox environment
+  // Remember that access tokens, products, etc. are completely separated between environments.
+  // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
+  server: "sandbox", /// todo: move this to .env or config file
+});
+
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
@@ -29,4 +46,25 @@ export const auth = betterAuth({
         .GITHUB_CLIENT_SECRET as string,
     },
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId:
+                "d4692ac4-fd8f-4ab7-a73a-66a1403f9627", // ID of Product from Polar Dashboard
+              slug: "Pro", // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+            },
+          ],
+          successUrl: "/success?checkout_id={CHECKOUT_ID}",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        usage(),
+      ],
+    }),
+  ],
 });
